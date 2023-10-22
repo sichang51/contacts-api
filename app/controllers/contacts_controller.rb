1d8bdcdede2f5c9fe2240fdcc84f1353317b11d0
sichang51 <99237899+sichang51@users.noreply.github.com>
2023-10-22 15:10:48 -0700

    correct answer with Kat's help - didn't add line after def

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,9 +1,11 @@
 class ContactsController < ApplicationController
   def first_contact
+    first_contact = Contact.first
     render json: { first_contact: first_contact }
   end
 
   def all_contacts
+    all_contacts = Contact.all
     render json: { contacts: all_contacts }
   end
 end
