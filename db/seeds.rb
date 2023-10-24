3a0cbda1b1eb8a291cc96f1ce9a598d668767dea
sichang51 <99237899+sichang51@users.noreply.github.com>
2023-10-23 19:26:14 -0700

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+contact = Contact.new(first_name: "Luke", last_name: "Skywalker", email: "force1@test.com", phone_number: "0123456789")
+contact.save
+
+contact = Contact.new(first_name: "Aragon", last_name: "Unknown", email: "strider@test.com", phone_number: "4567891234")
+contact.save
+
+contact = Contact.new(first_name: "Darth Vader", last_name: "Skywalker", email: "darkside1@test.com", phone_number: "7894561234")
+contact.save
+
+contact = Contact.new(first_name: "Legolas", last_name: "Elven", email: "forestelf@test.com", phone_number: "8881234567")
+contact.save
